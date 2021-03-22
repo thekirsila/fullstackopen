d15d2bd97aec46b3154c837f0d9e74d266a3834a
thekirsila <20385087+thekirsila@users.noreply.github.com>
2021-03-22 16:54:31 +0200

    osa4
    Tehtiin osasta 4 kaikki tehtävät

---
+++ osa4/blogilista/index.js
@@ -0,0 +1,13 @@
+const app = require('./app')
+const http = require('http')
+const config = require('./utils/config')
+const logger = require('./utils/logger')
+
+// const express = require('express')
+// const app = express()
+
+const server = http.createServer(app)
+
+server.listen(config.PORT, () => {
+  logger.info(`Server running on port ${config.PORT}`)
+})
