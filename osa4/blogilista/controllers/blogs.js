93e7037f3c60a83d2ef246aae036dfb00b5fd821
Miikka Kirsilä <miikka.kirsila@trinode.fi>
2023-02-11 15:21:06 +0200

    Part 4 Final changes

--- osa4/blogilista/controllers/blogs.js
+++ osa4/blogilista/controllers/blogs.js
@@ -1,7 +1,6 @@
 const blogsRouter = require('express').Router()
 const { isNull } = require('lodash')
 const Blog = require('../models/blog')
-const User = require('../models/user')
 const jwt = require('jsonwebtoken')
 
 blogsRouter.get('/', async (request, response) => {
--- osa4/blogilista/index.js
+++ osa4/blogilista/index.js
@@ -3,9 +3,6 @@ const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
 
-// const express = require('express')
-// const app = express()
-
 const server = http.createServer(app)
 
 server.listen(config.PORT, () => {
